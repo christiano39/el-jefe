a2f4ee9a1e0b1d693f3faf80ad44fc9a25ba31e2
christiano39 <carneson39@gmail.com>
2020-07-18 21:13:37 -0600

    added menu items

---
+++ src/data/menu.js
@@ -0,0 +1,387 @@
+const menu = {
+    breakfastBurritos: [
+        {
+            name: 'Egg and Potato Burrito',
+            price: '4.00',
+            description: ''
+        },
+        {
+            name: 'Egg, Potato, and Cheese Burrito',
+            price: '4.50',
+            description: ''
+        },
+        {
+            name: 'Egg, Potato, and Bacon Burrito',
+            price: '5.50',
+            description: ''
+        },
+        {
+            name: 'Egg, Potato, and Sausage Burrito',
+            price: '5.50',
+            description: ''
+        },
+        {
+            name: 'Egg, Potato, and Ham Burrito',
+            price: '5.50',
+            description: ''
+        },
+        {
+            name: 'Egg, Potato, and Chorizo Burrito',
+            price: '5.50',
+            description: ''
+        },
+        {
+            name: 'Egg, Potato, and Steak Burrito',
+            price: '6.75',
+            description: ''
+        },
+        {
+            name: 'Egg and Chorizo Burrito',
+            price: '6.50',
+            description: ''
+        },
+        {
+            name: 'Egg, Bean, and Chorizo Burrito',
+            price: '6.75',
+            description: ''
+        },
+        {
+            name: 'Egg, Potato, and Pork Burrito',
+            price: '5.50',
+            description: ''
+        },
+        {
+            name: 'Big Jefe DBL Breakfast Meat Burrito',
+            price: '8.00',
+            description: 'Our big breakfast burrito comes with double meat.'
+        },
+    ],
+    omelet: [
+        {
+            name: 'Egg Omelet',
+            price: '6.49',
+            description: 'Three egg omelette with your choice of meat.'
+        }
+    ],
+    quesadillas: [
+        {
+            name: 'Cheese Quesadilla',
+            price: '5.25',
+            description: ''
+        },
+        {
+            name: 'Shredded Pork or Beef Quesadilla',
+            price: '6.50',
+            description: ''
+        },
+        {
+            name: 'Chicken Quesadilla',
+            price: '7.00',
+            description: ''
+        },
+        {
+            name: 'Steak Quesadilla',
+            price: '8.00',
+            description: ''
+        },
+    ],
+    lunchDinnerBurritos: [
+        {
+            name: 'Bean Burrito',
+            price: '4.00',
+            description: ''
+        },
+        {
+            name: 'Bean and Cheese Burrito',
+            price: '4.50',
+            description: ''
+        },
+        {
+            name: 'Bean and Chicharron Burrito',
+            price: '5.50',
+            description: ''
+        },
+        {
+            name: 'Beef and Bean Burrito',
+            price: '5.50',
+            description: ''
+        },
+        {
+            name: 'Ground Beef Burrito',
+            price: '6.00',
+            description: ''
+        },
+        {
+            name: 'Shredded Pork Burrito',
+            price: '6.00',
+            description: ''
+        },
+        {
+            name: 'Shredded Beef Burrito',
+            price: '6.00',
+            description: ''
+        },
+        {
+            name: 'Chicken Breast Burrito',
+            price: '6.50',
+            description: ''
+        },
+        {
+            name: 'Steak Burrito',
+            price: '9.50',
+            description: ''
+        },
+        {
+            name: 'Chile Relleno Burrito',
+            price: '7.00',
+            description: ''
+        },
+    ],
+    tacos: [
+        {
+            name: 'Tacos',
+            price: '3.50',
+            description: 'Comes with choice of corn or flour tortillas and choice of meat.'
+        },
+        {
+            name: 'Mini Street Tacos',
+            price: '3.50',
+            description: 'Three mini tacos on corn tortillas with choice of meat.'
+        },
+    ],
+    chimichangaAndBurgers: [
+        {
+            name: 'Chimichanga',
+            price: '9.99',
+            description: 'Deep-fried tortilla stuffed with rice, beans, and your choice of meat smothered with your choice of mild, medium, or hot green chili on top.'
+        },
+        {
+            name: 'Mexican Hamburger',
+            price: '9.00',
+            description: 'Mexican burger. Served on flour tortilla smothered with beans and choice of mild, medium, or hot chili.'
+        },
+    ],
+    comboMeals: [
+        {
+            name: 'Egg and Potato Burrito Combo',
+            price: '4.99',
+            description: 'Comes with choice of drink. Comes with a side of mild, medium, or hot chili.'
+        },
+        {
+            name: 'Pancakes (3pcs) and Hash Browns Combo',
+            price: '4.99',
+            description: 'Comes with choice of drink.'
+        },
+        {
+            name: 'Taco and Tostada Combo',
+            price: '5.99',
+            description: 'Comes with choice of drink.'
+        },
+        {
+            name: 'French Toast and Hash browns Combo',
+            price: '5.99',
+            description: 'Comes with choice of drink.'
+        },
+        {
+            name: 'Small Chili Cheese Fries Combo',
+            price: '5.99',
+            description: 'Smothered with green chili. Comes with choice of drink.'
+        },
+        {
+            name: 'Nacho Supreme Combo',
+            price: '7.99',
+            description: 'Tortilla chips topped with ground beef, beans, lettuce, tomatoes, and melted cheese. Comes with choice of drink.'
+        },
+        {
+            name: 'Grilled Cheese and Fries Combo',
+            price: '6.99',
+            description: 'Comes with choice of drink.'
+        },
+        {
+            name: 'Tamales (2pcs) with Rice and Beans Combo',
+            price: '7.99',
+            description: 'Two pork tamales. Served with rice and beans. Comes with choice of drink.'
+        },
+        {
+            name: 'Burrito Supreme Combo',
+            price: '8.99',
+            description: 'Large burrito smothered with green chili (choose mild, medium, or hot) and your choice of meat. Comes with choice of drink.'
+        },
+        {
+            name: 'Large Chili Cheese Fries Combo',
+            price: '8.99',
+            description: 'Topped with green chili. Comes with choice of drink.'
+        },
+        {
+            name: 'Big El Jefe - Double Meat Burrito Combo',
+            price: '8.99',
+            description: 'Comes with choice of drink.'
+        },
+        {
+            name: 'Cheeseburger and Fries Combo',
+            price: '8.99',
+            description: 'Comes with choice of drink.'
+        },
+        {
+            name: 'Taco Salad Combo',
+            price: '8.99',
+            description: 'Comes with choice of drink.'
+        },
+        {
+            name: 'Mexican Hamburger Combo',
+            price: '9.99',
+            description: 'Mexican burger. Served on flour tortilla smothered with beans and choice of mild, medium, or hot chili. Comes with choice of drink.'
+        },
+        {
+            name: 'Big Daddy Breakfast Combo',
+            price: '8.99',
+            description: 'Hotcakes, 2 eggs (choice of scrambled, over easy, over medium, or over hard), choice of sausage, bacon, or hash browns. Comes with choice of drink.'
+        },
+        {
+            name: 'Huevos Rancheros Combo',
+            price: '10.99',
+            description: 'Eggs (choice of scrambled, over easy, over medium, or over hard), smothered with mild, medium, or hot chili. Served with rice and beans. Comes with choice of drink.'
+        },
+        {
+            name: 'Steak Burrito Dinner Combo',
+            price: '10.99',
+            description: 'Steak burrito comes with rice and beans. Choose your burrito preparation regular, smothered with green chili, deluxe smothered plus lettuce, and tomato. Comes with choice of drink.'
+        },
+        {
+            name: 'Menudo Combo',
+            price: '10.99',
+            description: 'Comes with choice of drink.'
+        },
+        {
+            name: 'Cheese Rellenos (2pcs), Beans, and Rice Combo',
+            price: '10.99',
+            description: 'Comes with choice of drink.'
+        },
+        {
+            name: 'Tacos Al Carbon Combo (3pcs)',
+            price: '11.99',
+            description: 'Choice of flour or corn tortilla. Comes with choice of drink.'
+        },
+        {
+            name: 'Cheese Enchiladas (2pcs), Beans, and Rice Combo',
+            price: '10.99',
+            description: 'Comes with choice of drink.'
+        },
+    ],
+    sides: [
+        {
+            name: 'Sopapillas',
+            price: '2.00',
+            description: ''
+        },
+        {
+            name: 'Chips',
+            price: '2.00',
+            description: ''
+        },
+        {
+            name: 'Rice',
+            price: '2.00',
+            description: ''
+        },
+        {
+            name: 'Salsa',
+            price: '2.00',
+            description: ''
+        },
+        {
+            name: 'Tostada with Ground Beef',
+            price: '2.00',
+            description: ''
+        },
+        {
+            name: 'Enchilada (1pcs)',
+            price: '3.50',
+            description: ''
+        },
+        {
+            name: 'Pork Tamale (1pcs)',
+            price: '3.00',
+            description: ''
+        },
+        {
+            name: 'Guacamole (20oz)',
+            price: '3.00',
+            description: ''
+        },
+        {
+            name: 'Chicharones',
+            price: '3.00',
+            description: ''
+        },
+        {
+            name: 'Chile Relleno (Cheese)',
+            price: '3.50',
+            description: ''
+        },
+        {
+            name: 'Beans',
+            price: '3.00',
+            description: ''
+        },
+        {
+            name: 'Chips and Salsa',
+            price: '5.00',
+            description: ''
+        },
+        {
+            name: 'Guacamole and Chips',
+            price: '7.00',
+            description: ''
+        },
+        {
+            name: 'Chili',
+            price: '6.00',
+            description: ''
+        },
+    ],
+    beverages: [
+        {
+            name: 'Pepsi',
+            price: '2.59',
+            description: ''
+        },
+        {
+            name: 'Diet Pepsi',
+            price: '2.59',
+            description: ''
+        },
+        {
+            name: 'Cherry Pepsi',
+            price: '2.59',
+            description: ''
+        },
+        {
+            name: 'Lemonade',
+            price: '2.59',
+            description: ''
+        },
+        {
+            name: 'Dr. Pepper',
+            price: '2.59',
+            description: ''
+        },
+        {
+            name: 'Sprite',
+            price: '2.59',
+            description: ''
+        },
+        {
+            name: 'Sierra Mist',
+            price: '2.59',
+            description: ''
+        },
+        {
+            name: 'Mountain Dew',
+            price: '2.59',
+            description: ''
+        },
+    ]
+};
+
+export default menu;
