4042932ffbf2a1dda0e3edd5ad0f338a61d264b7
christiano39 <carneson39@gmail.com>
2020-07-19 16:26:03 -0600

    added locations section

--- src/components/Location.js
+++ src/components/Location.js
@@ -4,7 +4,23 @@ const Location = props => {
     const { location } = props;
     return (
         <div className='location'>
-            
+            <address>
+                {location.address}<br/>
+                {`${location.city}, ${location.state} ${location.zip}`}<br/>
+                {`(${location.phone.slice(0, 3)}) ${location.phone.slice(3, 6)}-${location.phone.slice(6, 10)}`}
+            </address>
+            <div className='hours'>
+                <h3>Hours</h3>
+                <ul>
+                    {
+                        location.hours.map(item => {
+                            return item.close ?
+                            <li key={item.day}>{`${item.day}: ${item.open}-${item.close}`}</li>
+                            : <li key={item.day}>{`${item.day}: ${item.open}`}</li>
+                        })
+                    }
+                </ul>
+            </div>
         </div>
     )
 }
