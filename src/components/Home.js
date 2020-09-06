a23d94555030fb84be92530903c2100504b77ab9
christiano39 <carneson39@gmail.com>
2020-09-05 19:32:39 -0600

    add basic homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,33 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
+import { useHistory } from "react-router-dom";
 
 const Home = () => {
-    useEffect(() => {
-        document.title = "El Jefe | Home";
-        return () => {
-            document.title = "El Jefe"
-        }
-    }, [])
-    
-    return (
-        <section id='homepage'>
-            <h1>Welcome to El Jefe!</h1>
-        </section>
-    )
-}
+  const { push } = useHistory();
+
+  useEffect(() => {
+    document.title = "El Jefe | Home";
+    return () => {
+      document.title = "El Jefe";
+    };
+  }, []);
+
+  return (
+    <section id="homepage">
+      <h1>Welcome to El Jefe!</h1>
+      <div className="main">
+        <img
+          src="https://s3-media0.fl.yelpcdn.com/bphoto/3ftL8peRDZcFZuzyWysK-A/o.jpg"
+          alt="El Jefe storefront"
+        />
+        <div className="cta">
+          <h2>Get started by checking out our menu!</h2>
+          <button onClick={() => push("/menu")}>Menu</button>
+          <h2>Or find one of our locations!</h2>
+          <button onClick={() => push("/locations")}>Locations</button>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default Home;
